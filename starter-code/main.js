851a2edf8e69acaf593fdb1648e5b3db4a4faec4
Charith Perera <charith.p@gmail.com>
2016-08-21 20:18:38 +1000

    Unit 9 Assignment

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,3 +5,10 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardThree === cardFour) {
+	alert("You found a match!");
+}
+else {
+	alert("Sorry, try again!");
+}
