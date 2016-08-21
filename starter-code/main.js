26486d06abb8f4e8d1a70e7ce14b9e900b9f63b8
Charith Perera <charith.p@gmail.com>
2016-08-21 19:45:57 +1000

    Unit 8 Assignment

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,7 @@
 console.log("JS file is connected to HTML! Woo!")
+
+// create and initialize 4 card values
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
