1dc540ae12436263fec0bbfb087e3e66da6d7c01
Charith Perera <charith.p@gmail.com>
2016-08-22 17:08:41 +1000

    Unit 11 Assignment

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,31 +1,54 @@
-console.log("JS file is connected to HTML! Woo!")
-
-// create and initialize 4 card values
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-
-var maxCards = 4;
-
-// if (cardThree === cardFour) {
-// 	alert("You found a match!");
-// }
-// else {
-// 	alert("Sorry, try again!");
-// }
-
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
+var playerScore = 0;
 var gameBoard = document.getElementById("game-board");
 
-var createBoard = function(maxCards) {
+var resetBoard = function() {
+	allCards = document.getElementsByClassName("card");
+	for(var i = 0; i < allCards.length; i++) {
+		allCards[i].innerHTML = "";
+	}
+	cardsInPlay = [];
+}
 
-	for (var i = 0; i < maxCards; i++) {
+var isMatch =function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+		playerScore++;
+		document.getElementById("score").innerHTML = playerScore;
+		resetBoard();
+	}
+}
+
+var revealCard = function() {
+	var cardType = this.getAttribute('data-card');
+	this.innerHTML = "<img src='" + cardType + ".png'/>";
+	cardsInPlay.push(cardType);
+	if (cardsInPlay.length === 2) {
+		setTimeout(function() {
+			isMatch();
+		}, 100)
+	}
+	else if (cardsInPlay.length === 4) {
+		setTimeout(function(){
+			alert("You Lost!");
+			resetBoard();
+		}, 100)
+	}
+}
+
+var createBoard = function() {
+
+	for (var i = 0; i < cards.length; i++) {
 		var newCard = document.createElement("div");
 		newCard.className = "card";
+
+		newCard.setAttribute('data-card', cards[i]);
+		newCard.addEventListener('click', revealCard);
 
 		gameBoard.appendChild(newCard);
 	}
 
 }
 
-createBoard(maxCards);
+createBoard();
